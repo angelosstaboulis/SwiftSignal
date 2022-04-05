1173d9071a7d2e368b8ff236b6a7fb3a6dd4a67f
Angelos Staboulis <angelosstaboulis@gmail.com>
2022-04-06 02:52:52 +0300

    Initial Commit SwiftSignal(export Swift Class from libsignal-client to Objective C)

---
+++ SwiftSignal/SwiftSignal/SwiftSignal.h
@@ -0,0 +1,21 @@
+//
+//  SwiftSignal.h
+//  SwiftSignal
+//
+//  Created by Angelos Staboulis on 6/4/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftSignal.
+FOUNDATION_EXPORT double SwiftSignalVersionNumber;
+
+//! Project version string for SwiftSignal.
+FOUNDATION_EXPORT const unsigned char SwiftSignalVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftSignal/PublicHeader.h>
+
+@interface NativeHandleOwner
++ (NativeHandleOwner *)new;
+- (int)addNumbers;
+@end
